07755158787a6eb0ce51aba31a23ffebbaca7ba6
Thomas Hounsell <commits@thounsell.co.uk>
2013-06-27 14:05:17 +0100

    Initial Commit

---
+++ appxery/Controllers/appxController.cs
@@ -0,0 +1,127 @@
+﻿using Appxery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Appxery.Controllers
+{
+    public class appxController : Controller
+    {
+        //
+        // GET: /appx/
+
+        public ActionResult Index()
+        {
+            return View(AppXServer.AppXList.OrderBy(a => a.Name));
+        }
+
+        //
+        // GET: /appx/details/5
+
+        public ActionResult details(Guid id)
+        {
+            return View(AppXServer.AppXList.Single(s => s.AppxId == id));
+        }
+
+        //
+        // GET: /appx/download/5
+
+        public ActionResult download(Guid id)
+        {
+            AppX app = AppXServer.AppXList.Single(s => s.AppxId == id);
+            Response.ContentType = "application/vnd.ms-appx";
+            Response.AddHeader("Content-Disposition", string.Format("attachment; filename=\"{0}.appx\"", app.Name));
+            Response.WriteFile(app.FullPath);
+            Response.Flush();
+            return null;
+        }
+
+        //
+        // GET: /appx/refresh/
+        public ActionResult refresh()
+        {
+            AppXServer.ImportAppxs();
+            return RedirectToAction("Index");
+        }
+
+        ////
+        //// GET: /appx/Create
+
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /appx/Create
+
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        ////
+        //// GET: /appx/Edit/5
+
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /appx/Edit/5
+
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        ////
+        //// GET: /appx/Delete/5
+
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /appx/Delete/5
+
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ appxery/Models/AppX.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Appxery.Models
+{
+    [Serializable]
+    public class AppX
+    {
+        public Guid AppxId { get; set; }
+
+        public long Size { get; set; }
+        public string FullPath { get; set; }
+
+        public string Name { get; set; }
+        public Version Version { get; set; }
+        public string Publisher { get; set; }
+        public Architecture ProcessorArchitecture { get; set; }
+
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+        public string PublisherDisplayName { get; set; }
+
+        public Version OSMinVersion { get; set; }
+        public Version OSMaxVersionTested { get; set; }
+    }
+
+    public enum Architecture
+    {
+        x86,
+        x64,
+        arm,
+        neutral
+    }
+
+
+    public static class AppXServer
+    {
+        static AppXServer()
+        {
+            ImportAppxs();
+        }
+
+        public static void ImportAppxs()
+        {
+            AppXList = new List<AppX>();
+
+            string importPath = HttpContext.Current.Server.MapPath("~/App_Data/AppX-Import");
+            string storePath = HttpContext.Current.Server.MapPath("~/App_Data/AppX-Store");
+            
+            DirectoryInfo di = new DirectoryInfo(importPath);
+
+            FileInfo[] appxs = di.GetFiles("*.appx");
+
+            foreach (FileInfo appx in appxs)
+            {
+                ZipArchive za = ZipFile.OpenRead(appx.FullName);
+                var appxManifest = za.Entries.First(f => f.Name == "AppxManifest.xml");
+                string xmlPath = Path.Combine(importPath, appx.Name.Substring(0, appx.Name.Length - appx.Extension.Length) + ".xml");
+
+                if (!File.Exists(xmlPath))
+                {
+                    appxManifest.ExtractToFile(xmlPath);
+                }
+
+                za.Dispose();
+                FileStream appxManifestStr = new FileStream(xmlPath, FileMode.Open);
+                XDocument appxManifestXml = XDocument.Load(appxManifestStr);
+
+                AppX item = new AppX()
+                {
+                    AppxId = Guid.NewGuid(),
+
+                    Size = appx.Length,
+
+                    Name = appxManifestXml.Root.LocalElement("Identity").Attribute("Name").Value,
+                    Version = Version.Parse(appxManifestXml.Root.LocalElement("Identity").Attribute("Version").Value),
+                    Publisher = appxManifestXml.Root.LocalElement("Identity").Attribute("Publisher").Value,
+                    ProcessorArchitecture = (Architecture)Enum.Parse(typeof(Architecture), appxManifestXml.Root.LocalElement("Identity").Attribute("ProcessorArchitecture").Value),
+
+                    DisplayName = appxManifestXml.Root.LocalElement("Properties").LocalElement("DisplayName").Value,
+                    Description = appxManifestXml.Root.LocalElement("Properties").LocalElement("Description") != null ? appxManifestXml.Root.LocalElement("Properties").LocalElement("Description").Value : "",
+                    PublisherDisplayName = appxManifestXml.Root.LocalElement("Properties").LocalElement("PublisherDisplayName").Value,
+
+                    OSMinVersion = Version.Parse(appxManifestXml.Root.LocalElement("Prerequisites").LocalElement("OSMinVersion").Value),
+                    OSMaxVersionTested = Version.Parse(appxManifestXml.Root.LocalElement("Prerequisites").LocalElement("OSMaxVersionTested").Value)
+                };
+
+                appxManifestStr.Flush();
+                appxManifestStr.Close();
+
+                File.Delete(xmlPath);
+
+                // Import and Store details
+                DirectoryInfo storeDir = Directory.CreateDirectory(Path.Combine(storePath, item.AppxId.ToString()));
+                File.Move(appx.FullName, Path.Combine(storeDir.FullName, appx.Name));
+                item.FullPath = Path.Combine(storeDir.FullName, appx.Name);
+                BinaryFormatter bf = new BinaryFormatter();
+                FileStream fStr = new FileStream(Path.Combine(storeDir.FullName, "data.bin"), FileMode.CreateNew);
+                bf.Serialize(fStr, item);
+                fStr.Flush();
+                fStr.Close();
+            }
+
+            foreach (DirectoryInfo storedAppx in new DirectoryInfo(storePath).GetDirectories())
+            {
+                FileInfo[] storeBins = storedAppx.GetFiles("data.bin");
+                FileInfo storeBin = storeBins.First();
+
+                FileStream storeBinStr = storeBin.OpenRead();
+                BinaryFormatter bf = new BinaryFormatter();
+                AppX item = bf.Deserialize(storeBinStr) as AppX;
+
+                if (item != null)
+                {
+                    AppXList.Add(item);
+                }
+
+                storeBinStr.Flush();
+                storeBinStr.Close();
+            }
+        }
+
+        public static List<AppX> AppXList { get; set; }
+
+        public static XElement LocalElement(this XElement element, XName elementName)
+        {
+            return element.Elements().SingleOrDefault(s => s.Name.LocalName == elementName);
+        }
+    }
+}
